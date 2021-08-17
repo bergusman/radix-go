ef1bd9247a804a799df2cf35e19a2705983fc89d
Vitaly Berg <bergusman@gmail.com>
2021-08-17 20:55:27 +0800

    Implement Base58

--- base58.go
+++ base58.go
@@ -1,13 +1,50 @@
 package radix
 
+import (
+	"errors"
+	"unicode/utf8"
+)
+
+var ErrBase58BadAlphabet = errors.New("base58: alphabet length less than 58")
+
 const AlphabetBitcoin = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 const AlphabetRipple = "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz"
 const AlphabetFlickr = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
 
-func EncodeBase58(input []byte, alphabet string) (string, error) {
-	return "", nil
+func Base58Encode(input []byte, alphabet string) (string, error) {
+	if utf8.RuneCountInString(alphabet) < 58 {
+		return "", ErrBase58BadAlphabet
+	}
+
+	input58, err := ConvertBytes(input, 58)
+	if err != nil {
+		return "", err
+	}
+
+	zeror, _ := utf8.DecodeRuneInString(alphabet)
+	zero := string(zeror)
+	padding := ""
+	for _, in := range input {
+		if in == 0 {
+			padding += zero
+		} else {
+			break
+		}
+	}
+
+	output, err := EncodeBytes(input58, alphabet)
+	if err != nil {
+		return "", err
+	}
+	if len(padding) == 0 {
+		return output, nil
+	}
+	return padding + output, nil
 }
 
-func DecodeBase58(input string) ([]byte, error) {
-	return nil, nil
+func Base58Decode(input string, alphabet string) ([]byte, error) {
+	if utf8.RuneCountInString(alphabet) < 58 {
+		return nil, ErrBase58BadAlphabet
+	}
+	return DecodeBytes(input, alphabet)
 }
--- base58_test.go
+++ base58_test.go
@@ -1,1 +1,61 @@
 package radix
+
+import (
+	"reflect"
+	"testing"
+)
+
+var base58Tests = []struct {
+	Bytes  []byte
+	String string
+}{
+	{Bytes: []byte{255, 255, 255, 255}, String: "7YXq9G"},
+	{Bytes: []byte{0, 0, 0, 0, 255, 255, 255, 255}, String: "11117YXq9G"},
+	{Bytes: []byte{0, 1, 2, 3, 4}, String: "12VfUX"},
+}
+
+func TestBase58Encode(t *testing.T) {
+	for _, tt := range base58Tests {
+		output, err := Base58Encode(tt.Bytes, AlphabetBitcoin)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if output != tt.String {
+			t.Errorf("got: %v; want: %v", output, tt.String)
+		}
+	}
+}
+
+func TestBase58EncodeErrors(t *testing.T) {
+	_, err := Base58Encode([]byte{}, "1234")
+	if err != nil {
+		if err != ErrBase58BadAlphabet {
+			t.Errorf("got: %v; want: ErrBase58BadAlphabet", err)
+		}
+	} else {
+		t.Error("want not nit err")
+	}
+}
+
+func TestBase58Decode(t *testing.T) {
+	for _, tt := range base58Tests {
+		output, err := Base58Decode(tt.String, AlphabetBitcoin)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if reflect.DeepEqual(output, tt.Bytes) {
+			t.Errorf("got: %v; want: %v", output, tt.Bytes)
+		}
+	}
+}
+
+func TestBase58DecodeErrors(t *testing.T) {
+	_, err := Base58Decode("", "1234")
+	if err != nil {
+		if err != ErrBase58BadAlphabet {
+			t.Errorf("got: %v; want: ErrBase58BadAlphabet", err)
+		}
+	} else {
+		t.Error("want not nit err")
+	}
+}
